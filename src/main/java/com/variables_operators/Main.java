79bf4fc099b56d7e0cd89bdcafb756d37aeaf11f
RudnichenkoML <maximrudnichenko@gmail.com>
2022-08-23 14:52:21 +0300

    add homework Variables and operators topic

--- src/main/java/com/variables_operators/Main.java
+++ src/main/java/com/variables_operators/Main.java
@@ -1,6 +1,30 @@
 package com.variables_operators;
 
 public class Main {
+
+
     public static void main(String[] args) {
+
+        printMessage("Hi! my name is Max");
+        printSum(2, 9);
+        System.out.println(multiply(2, 4));
+
+
     }
+
+    public static void printMessage(String string) {
+
+        System.out.println(string);
+    }
+
+    public static void printSum(int a, int b) {
+        System.out.println(a + "+" + b + " = " + (a + b));
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+
 }
+
