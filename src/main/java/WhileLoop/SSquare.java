8151dfb62d5d06604782466a8cfb82ab2dfc1181
RudnichenkoML <maximrudnichenko@gmail.com>
2022-08-29 14:00:35 +0300

    add fix to SSquare class

--- src/main/java/WhileLoop/SSquare.java
+++ src/main/java/WhileLoop/SSquare.java
@@ -2,13 +2,15 @@ package WhileLoop;
 
 public class SSquare {
     public static void main(String[] args) {
-        int w = 10;
+        int x = 10;
         do {
             for (int i = 0; i<10; i++){
                 System.out.print("S");
             }
             System.out.println();
-        }while (w-->1);
+            x--;
+        }while (x>1);
+
 
     }
 }
